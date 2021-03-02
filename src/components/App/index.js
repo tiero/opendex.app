2d21e1145450a567661a6c5038245a1cdd8cc344
maybeast <78227110+maybeast@users.noreply.github.com>
2021-03-02 08:43:26 -0500

    fix global theme provider

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,34 +1,74 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Submarine from '../Submarine';
-import { fetchPairs } from '../../actions/submarineActions';
-import { ThemeProvider } from "@material-ui/styles";
-import './style.scss';
 import { createMuiTheme } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import "../../styles/global.scss";
+import Homepage from "../../views/homepage";
+import { renderRoutes } from "../../services/route/routeService";
+import { createStore } from "redux";
+import rootReducer from "../../combineReducers";
+import createBrowserHistory from "history/createBrowserHistory";
+import routes from "../../services/route/routes";
+
+//providers
+import { UtilsProvider } from "../../context/UtilsContext";
+import { StepsProvider } from "../../context/StepsContext";
 
 export const opendexTheme = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: '#f15a24',
+      main: "#0c0c0c",
+    },
+    secondary: {
+      main: "#f15a24",
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        html: {
+          WebkitFontSmoothing: "auto",
+          padding: "0",
+          margin: "0",
+        },
+        body: {
+          color: "#f2f2f2",
+          "background-color": "#0c0c0c",
+        },
+        a: {
+          color: "#f15a24",
+          "text-decoration": "none",
+        },
+      },
     },
   },
 });
 
-function App({ dispatch }) {
-  dispatch(fetchPairs());
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
+const history = createBrowserHistory();
+
+function App() {
   return (
     <ThemeProvider theme={opendexTheme}>
       <CssBaseline />
-      <div className="App">
-        <header className="App-header">
-          <Submarine />
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router history={history}>
+          <UtilsProvider>
+            <StepsProvider>
+              <Homepage>{renderRoutes(routes)}</Homepage>
+            </StepsProvider>
+          </UtilsProvider>
+        </Router>
+      </Provider>
     </ThemeProvider>
   );
 }
 
-export default connect()(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,75 +1,45 @@
-import is from 'is_js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
-import createBrowserHistory from 'history/createBrowserHistory';
-import './index.scss';
-import Homepage from './views/homepage';
-import rootReducer from './combineReducers';
-import { faqUrl } from './constants/environment';
-import * as serviceWorker from './serviceWorker';
-import routes from '../src/services/route/routes';
-import { renderRoutes } from './services/route/routeService';
+import { Typography } from "@material-ui/core";
+import is from "is_js";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.scss";
+import * as serviceWorker from "./serviceWorker";
+import routes from './services/route/routes';
 
-//providers
-import { UtilsProvider } from './context/UtilsContext';
-import { StepsProvider } from './context/StepsContext';
-
-const isSupportedBrowser = is.chrome() || is.firefox() || is.safari() || is.ios();
-
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-const history = createBrowserHistory();
+const isSupportedBrowser =
+  is.chrome() || is.firefox() || is.safari() || is.ios();
 
 const Layout = () => {
-    // Redirect the "/faq" page to the Notion document
-    if (window.location.pathname.endsWith("/faq")) {
-        window.location.replace(faqUrl);
-    }
-
-
-    if (!routes.map(r => r.path).includes(window.location.pathname)) {
-        return (
-            <div className="homepage_error_wrapper">
-                <Typography variant="h3" component="h2" align="center">404 - Not Found</Typography>
-                <Typography variant="div" component="h2" align="center">The path you are trying to access does not exist</Typography>
-            </div>
-        )
-    }
-
-    if (isSupportedBrowser) {
-        return (
-            <Provider store={store}>
-                <Router history={history}>
-                    <UtilsProvider>
-                        <StepsProvider>
-                            <Homepage>
-                                {renderRoutes(routes)}
-                            </Homepage>
-                        </StepsProvider>
-                    </UtilsProvider>
-                </Router>
-            </Provider>
-        );
-    }
+  if (!routes.map((r) => r.path).includes(window.location.pathname)) {
     return (
-        <div className="homepage_error_wrapper">
-            <Typography variant="h3" component="h2" align="center">Browser not supported!</Typography>
-            <Typography variant="div" component="h2" align="center">Please use one of the following browsers: Chrome, Firefox or Safari</Typography>
-        </div>
-    )
+      <div className="homepage_error_wrapper">
+        <Typography variant="h3" component="h2" align="center">
+          404 - Not Found
+        </Typography>
+        <Typography variant="div" component="h2" align="center">
+          The path you are trying to access does not exist
+        </Typography>
+      </div>
+    );
+  }
 
-}
+  if (isSupportedBrowser) {
+    return <App />;
+  }
 
-ReactDOM.render(
-    <Layout />,
-    document.getElementById('root')
-);
+  return (
+    <div className="homepage_error_wrapper">
+      <Typography variant="h3" component="h2" align="center">
+        Browser not supported!
+      </Typography>
+      <Typography variant="div" component="h2" align="center">
+        Please use one of the following browsers: Chrome, Firefox or Safari
+      </Typography>
+    </div>
+  );
+};
+
+ReactDOM.render(<Layout />, document.getElementById("root"));
 
 serviceWorker.register();
