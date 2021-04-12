8416e98d96a1ca492ecb6481c5f1285a93d47c80
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-04-13 00:12:54 +0400

    rates fetcher interface

---
+++ src/constants/rates.ts
@@ -0,0 +1,31 @@
+import CurrencyID from './currency';
+
+export interface AmountCurrency {
+  amount: number;
+  currency: CurrencyID;
+}
+
+export interface Preview {
+  //This is comprhensive of swap provider fees
+  amountWithFees: AmountCurrency;
+  // This is general purpose key value object that can be used to communicate
+  // to end users the detail of amounts added into the amountWithFees
+  feesDetail?: Record<string, any>;
+}
+
+export interface RatesFetcher {
+  // Preview wants the amount and the currency entered by the user and will return the
+  // amount to be sent or to be received of the opposite currency in the pair, comprhensive of fees.
+  // By default it "should" expect the "sending amount" to be given, but also the "receiving amount" could be
+  // passed setting the isSend parameter to false in order to do a reverse calculation.
+  Preview(
+    amountWithCurrency: AmountCurrency,
+    isSend: boolean
+  ): Promise<Preview>;
+
+  // PreviewGivenSend does the same thing as Preview with isSend = true
+  PreviewGivenSend(amountWithCurrency: AmountCurrency): Promise<Preview>;
+
+  // PreviewGivenReceive does the same thing as Preview with isSend = false
+  PreviewGivenReceive(amountWithCurrency: AmountCurrency): Promise<Preview>;
+}
---
+++ src/constants/rates_example.ts
@@ -0,0 +1,53 @@
+import CurrencyID from './currency';
+import { RatesFetcher, Preview, AmountCurrency } from './rates';
+
+export default class ExampleFetcher implements RatesFetcher {
+  private client: any;
+
+  constructor() {
+    this.client = window.fetch;
+  }
+
+  async Preview(
+    amountWithCurrency: AmountCurrency,
+    isSend: boolean = true
+  ): Promise<Preview> {
+    const usdPerBtc = await this.fetchPriceBTC();
+
+    const isBTCcomingIn =
+      (isSend && amountWithCurrency.currency === CurrencyID.LIQUID_BTC) ||
+      (!isSend && amountWithCurrency.currency === CurrencyID.LIQUID_USDT);
+
+    const amount = isBTCcomingIn
+      ? usdPerBtc * amountWithCurrency.amount
+      : amountWithCurrency.amount / usdPerBtc;
+    const currency = isBTCcomingIn
+      ? CurrencyID.LIQUID_USDT
+      : CurrencyID.LIQUID_BTC;
+    return {
+      amountWithFees: {
+        amount: amount,
+        currency: currency,
+      },
+    };
+  }
+
+  // PreviewGivenSend does the same thing as Preview with isSend = true
+  PreviewGivenSend(amountWithCurrency: AmountCurrency): Promise<Preview> {
+    return this.Preview(amountWithCurrency, true);
+  }
+
+  // PreviewGivenReceive does the same thing as Preview with isSend = false
+  PreviewGivenReceive(amountWithCurrency: AmountCurrency): Promise<Preview> {
+    return this.Preview(amountWithCurrency, false);
+  }
+
+  async fetchPriceBTC(): Promise<number> {
+    const res = await this.client(
+      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+    );
+    const json = await res.json();
+
+    return json.bitcoin.usd as number;
+  }
+}
